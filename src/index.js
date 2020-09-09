d171d15b9fe890f2cc040c73aa244899870be4cc
Tanishq Mondal <52147425+tanishq-stark@users.noreply.github.com>
2020-09-10 00:42:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import NameForm from './NameForm'
 import * as serviceWorker from './serviceWorker';
 
+
+
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client'));
+
+server.listen(PORT, function() {
+  console.log('Chat server running');
+}); 
+
 ReactDOM.render(
     <div>
     <NameForm />
